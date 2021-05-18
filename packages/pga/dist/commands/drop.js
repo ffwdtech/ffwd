c9065cdec267eb0f834b39ca0a363636aef996c9
Juho Hietala <hietala@gmail.com>
2021-05-18 04:25:30 +0200

    dist

--- packages/pga/dist/commands/drop.js
+++ packages/pga/dist/commands/drop.js
@@ -3,7 +3,7 @@ import { db, sql } from '@ffwd/db';
 export async function dropCmd(tables) {
     console.log('Dropping tables:', tables);
     await asyncForEach(tables, async (table) => {
-        const sqlQuery = sql `DROP TABLE ${table} CASCADE`;
+        const sqlQuery = sql.__dangerous__rawValue(`DROP TABLE ${table} CASCADE`);
         console.log(sqlQuery);
         await db.query(sqlQuery);
         console.log('Done!');
